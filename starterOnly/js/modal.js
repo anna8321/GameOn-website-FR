c86f045fdc5b9c0ba02954cd1bb168fa38b29578
anais meirone <anais.meirone@live.fr>
2022-06-29 12:00:29 +0200

    correcting html, adding for and id for label and input, deleting double class, changing form valide messages, adding close button function

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
+const openConfirmModal = document.querySelector(".open-confirm-modal");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeModalBtn.addEventListener("click", closeModal);
+openConfirmModal
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
